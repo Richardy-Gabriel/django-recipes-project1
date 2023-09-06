86d1e9f8a269e3434dea87edf4c0908b784f340d
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2023-09-06 10:03:28 -0300

    imported font-awwsome

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home(request):
-    return render(request, 'home.html', {
+    return render(request, 'recipes/home.html', {
         'name': 'Richardy Gabriel'
     })
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
