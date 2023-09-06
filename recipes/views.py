159967b19712dde33e30df72963e0cc3eef4f686
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2023-09-06 09:49:05 -0300

    settings modificaded

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'home.html', {
+        'name': 'Richardy Gabriel'
+    })
 
 
 def contato(request):
