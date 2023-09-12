45a58b7c5afa4b1d9f15a3ea8387c7f0032fefee
Richardy Gabriel Rodrigues da Costa <richardyrodrigues519@gmail.com>
2023-09-12 11:29:58 -0300

    real data inserted, views, urls created

--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
+from .models import Recipe
 
 
 def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
     return render(request, 'recipes/pages/home.html', {
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes
     })
 
 
@@ -12,4 +16,14 @@ def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', {
         'recipe': make_recipe(),
         'is_detail_page': True,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', {
+        'recipes': recipes
     })
